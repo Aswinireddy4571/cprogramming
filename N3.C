335fe4688e474ec3cf27d85dd574593361e698c1
Aswini reddy4571 <122337756+Aswinireddy4571@users.noreply.github.com>
2023-02-01 00:59:31 -0800

    Create N3.C

---
+++ N3.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int dividend,divisor,quotient,remainder
+printf("enter divided:");
+scanf("%d",&divisior);
+//computer quotient	
+quotient=dividend/divisor;
+//computers remainder
+remainder=dividend%divisor;
+printf("quotient=%d\n",quotient);
+printf("remainder=%d",remainder);
+return 0;
+}
